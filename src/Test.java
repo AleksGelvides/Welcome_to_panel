a82db4750438b3c930cede4a59fcce12035f34ef
AGCPA <60176859+AGCPA@users.noreply.github.com>
2021-05-15 23:12:30 +0700

    Работает чётко. Можно интегрировать в основной код

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,16 @@
 import java.util.Arrays;
+import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) {
-        String enAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String log = "Alex";
-        String pass = "Gel1995e";
+        String enAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String log = "abc";
+        String pass = "sdfsdfsdf";
         int shift = 3;
-        StringBuilder fullencrypt = new StringBuilder(log.concat(pass).length());
+        StringBuilder fullencrypt = new StringBuilder(pass.toUpperCase());
         for (int i = 0; i < fullencrypt.length(); i++){
-            fullencrypt.setCharAt(i, enAlphabet.charAt(enAlphabet.charAt(Arrays.binarySearch)));
-            System.out.println(fullencrypt);
+                fullencrypt.setCharAt(i, enAlphabet.charAt(enAlphabet.indexOf(String.valueOf(fullencrypt.charAt(i)))+shift));
         }
+        System.out.println(fullencrypt);
     }
 }
