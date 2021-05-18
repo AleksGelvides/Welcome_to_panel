9ad3c7ec75fcafb08ea3eb948b31fcebd705b445
AGCPA <60176859+AGCPA@users.noreply.github.com>
2021-05-18 19:28:01 +0700

    Роботает

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     private static String log;
-    private static String pass;
     private static int shift;
+    private static StringBuilder savedEnPass = new StringBuilder();
     public static void main(String[] args) {
-        System.out.println("Зарегестрируйтесь или залогиньтесь в системе");
-        System.out.println("Введите \"Login\" для входа или \"Reg\" для регистрации");
-        Scanner scanner = new Scanner(System.in);
-        String newLogin = scanner.nextLine();
-        if (newLogin.equalsIgnoreCase("login")) Login();
-        if (newLogin.equalsIgnoreCase("reg")) Registration();
+        boolean adminPanel = true;
+        while (adminPanel) {
+            System.out.println("Зарегистрируйтесь или залогиньтесь в системе");
+            System.out.println("Введите \"Log\" для входа или \"Reg\" для регистрации");
+            Scanner scanner = new Scanner(System.in);
+            String newLogin = scanner.nextLine();
+            if (newLogin.equalsIgnoreCase("log")) login();
+            if (newLogin.equalsIgnoreCase("reg")) registration();
+        }
     }
-    public static void Login(){
-
-    }
-    public static void Registration(){
+    public static void login(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Вход в панель");
+        System.out.println("Введите логин и пароль через Enter");
+        for (int i = 9; i >= 0; i--) {
+            String enterLogin = scanner.nextLine();
+            String enterPass = scanner.nextLine();
+            if (enterLogin.equals(log) && enterPass.equalsIgnoreCase(String.valueOf(decrypt(savedEnPass)))){
+                System.out.println("Добро пожаловать в админку :)");
+                break;
+            }  else {
+                System.out.println("Неверный логин или пароль, повторите попытку. Осталось попыток: " + i);
+                }
+        }
+    }
+    public static void registration(){
+        Scanner scanner = new Scanner(System.in);
+                String pass = null;
                 boolean logcy = true;
                 while (logcy) {
                     System.out.print("Придумайте и введите ваш логин (До 10 символов): ");
@@ -48,14 +64,24 @@ public class Main {
             } else
                 break;
         }
+        savedEnPass = encrypt(log,pass,shift);
     }
-    private static StringBuilder Encrypt (String login, String password, int ciphershift){
+    private static StringBuilder encrypt (String login, String password, int ciphershift){
         String enAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        StringBuilder fullencrypt = new StringBuilder(pass.toUpperCase());
+        StringBuilder fullencrypt = new StringBuilder(password.toUpperCase());
         for (int i = 0; i < fullencrypt.length(); i++){
             if(enAlphabet.lastIndexOf(fullencrypt.charAt(i))+shift >= enAlphabet.length()) enAlphabet = enAlphabet.concat(enAlphabet);
             fullencrypt.setCharAt(i, enAlphabet.charAt(enAlphabet.indexOf(String.valueOf(fullencrypt.charAt(i)))+shift));
         }
         return fullencrypt;
+    }
+    private static StringBuilder decrypt (StringBuilder password){
+        String enAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder checkPass = new StringBuilder(password);
+        for (int i = 0; i < checkPass.length(); i++){
+            if(enAlphabet.lastIndexOf(checkPass.charAt(i))-shift <= 0) enAlphabet = enAlphabet.concat(enAlphabet);
+            checkPass.setCharAt(i, enAlphabet.charAt(enAlphabet.indexOf(String.valueOf(checkPass.charAt(i)))-shift));
+        }
+        return checkPass;
     }
 }
