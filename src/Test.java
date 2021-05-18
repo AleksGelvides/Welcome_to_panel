55d5e433a7871119696e445783a96ef76a3ac98a
AGCPA <60176859+AGCPA@users.noreply.github.com>
2021-05-18 19:29:09 +0700

    Работает

--- src/Test.java
+++ src/Test.java
@@ -3,14 +3,15 @@ import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) {
-        String enAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder enAlphabet = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         String log = "abc";
-        String pass = "kflkdb";
+        String pass = "aaa";
         int shift = 3;
         StringBuilder fullencrypt = new StringBuilder(pass.toUpperCase());
-        for (int i = 0; i < fullencrypt.length(); i++){
-                if(enAlphabet.lastIndexOf(fullencrypt.charAt(i))+shift >= enAlphabet.length()) enAlphabet = enAlphabet.concat(enAlphabet);
-                fullencrypt.setCharAt(i, enAlphabet.charAt(enAlphabet.indexOf(String.valueOf(fullencrypt.charAt(i)))+shift));
+        for (int i = 0; i < fullencrypt.length(); i++) {
+            if (enAlphabet.lastIndexOf(String.valueOf(fullencrypt.charAt(i))) + shift >= enAlphabet.length())
+                enAlphabet = enAlphabet.append(enAlphabet);
+            fullencrypt.setCharAt(i, enAlphabet.charAt(enAlphabet.indexOf(String.valueOf(fullencrypt.charAt(i))) + shift));
         }
         System.out.println(fullencrypt);
     }
